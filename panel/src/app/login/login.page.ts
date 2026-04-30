77bc20d4ec8b0c03235206480cdf2f3021dbea4d
Audrei Plume <138030967+ThePlumedOne@users.noreply.github.com>
2026-04-30 22:54:32 +0000

    style login page (wip)

--- panel/src/app/login/login.page.ts
+++ panel/src/app/login/login.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonButton, IonIcon, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
 
