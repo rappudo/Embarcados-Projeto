7ff66064577a08fa5654b097cf49f689a24fc0b0
Audrei Plume <semideiaa0@gmail.com>
2026-05-03 23:55:03 -0300

    fix login page errors

--- panel/src/app/login/login.page.ts
+++ panel/src/app/login/login.page.ts
@@ -1,37 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-<<<<<<< Updated upstream
-import { IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
-import { IonHeader, IonToolbar, IonButton, IonIcon, IonContent } from '@ionic/angular/standalone';
-=======
 import { RouterLink } from '@angular/router';
 import { IonContent } from '@ionic/angular/standalone';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-<<<<<<< Updated upstream
-  imports: 
-  [IonHeader, 
-    IonToolbar, 
-    IonButton, 
-    IonIcon, 
-    CommonModule, 
-    FormsModule,
-    IonContent,
-    IonButton,
-    FormsModule,
-    IonItem,
-    IonLabel,
-    IonInput
-  ]
-=======
   imports: [IonContent, CommonModule, FormsModule, RouterLink],
->>>>>>> Stashed changes
 })
 export class LoginPage implements OnInit {
   email = '';
