9a8ed5aecb955ff5773417b8c245e0704ab69398
Audrei Plume <semideiaa0@gmail.com>
2026-05-04 00:55:52 -0300

    finish cadastro page

--- panel/src/app/cadastro/cadastro.page.ts
+++ panel/src/app/cadastro/cadastro.page.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonItem, IonLabel, IonInput, IonCheckbox } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonButton,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonCheckbox,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class CadastroPage implements OnInit {
+  nome = '';
+  sobrenome = '';
+  telefone = '';
+  empresa = '';
+  email = '';
 
-  constructor() { }
+  agreedToTerms = false;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  submitCadastro() {
+    // Por enquanto, apenas limpa os campos (a persistência/validação server-side pode ser integrada depois).
+    this.nome = '';
+    this.sobrenome = '';
+    this.telefone = '';
+    this.empresa = '';
+    this.email = '';
+    this.agreedToTerms = false;
   }
-
 }
