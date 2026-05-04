45506076637102ab6b0abbd9b7dc19a697e87248
Audrei Plume <semideiaa0@gmail.com>
2026-05-03 23:46:19 -0300

    finish login screen

--- panel/src/app/login/login.page.ts
+++ panel/src/app/login/login.page.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+<<<<<<< Updated upstream
 import { IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 import { IonHeader, IonToolbar, IonButton, IonIcon, IonContent } from '@ionic/angular/standalone';
+=======
+import { RouterLink } from '@angular/router';
+import { IonContent } from '@ionic/angular/standalone';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
+<<<<<<< Updated upstream
   imports: 
   [IonHeader, 
     IonToolbar, 
@@ -23,12 +29,21 @@ import { IonHeader, IonToolbar, IonButton, IonIcon, IonContent } from '@ionic/an
     IonLabel,
     IonInput
   ]
+=======
+  imports: [IonContent, CommonModule, FormsModule, RouterLink],
+>>>>>>> Stashed changes
 })
 export class LoginPage implements OnInit {
+  email = '';
+  password = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  submitLogin() {
+    // Por enquanto, apenas limpa os campos (a autenticação pode ser integrada depois).
+    this.email = '';
+    this.password = '';
   }
-
 }
