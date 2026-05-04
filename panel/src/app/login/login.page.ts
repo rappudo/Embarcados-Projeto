54c2aaae93bb8c83540a890e2e9b24f887c3ceb3
Audrei Plume <semideiaa0@gmail.com>
2026-05-04 00:24:45 -0300

    fix border login page fields

--- panel/src/app/login/login.page.ts
+++ panel/src/app/login/login.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, RouterLink],
+  imports: [IonContent, IonButton, IonItem, IonLabel, IonInput, CommonModule, FormsModule, RouterLink],
 })
 export class LoginPage implements OnInit {
   email = '';
